b3e73a2fa130311afd5e373cb78c8b71cc217dcc
Liya Tony <98206425+liyatony@users.noreply.github.com>
2024-12-28 12:06:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default defineConfig({
-  base: '/<MeetMyHelper._>/', // Replace <repository-name> with your repo name
+  base: '/MeetMyHelper._/', // Replace <repository-name> with your repo name
+  build:{
+    outDir: 'dist',
+  },
 });
